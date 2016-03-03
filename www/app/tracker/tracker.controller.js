fd542fe95ea9b508ddfba5f9848fb7509914b3e4
Pieter-Jan Vandenberghe <pieterjan.vandenberghe.t6365@student.hogent.be>
2016-03-03 16:48:53 +0100

    Resolved #2 leaflet polylines to draw route

--- www/app/tracker/tracker.controller.js
+++ www/app/tracker/tracker.controller.js
@@ -5,16 +5,53 @@
     .module('app.tracker')
     .controller('TrackerController', Controller);
 
-  Controller.$inject = []; //dependencies
+  Controller.$inject = ['$scope', 'leafletData']; //dependencies
 
   /* @ngInject */
-  function Controller(dependencies) {
+  function Controller($scope, leafletData) {
     var vm = this;
+    angular.extend($scope, {
+      gent: {
+        lat: 55, //51.050,
+        lng: 4, //3.733,
+        zoom: 10
+      },
+      markers: []
+    });
 
-    activate();
+    var clearRoutes = function() {
+      $scope.paths = {};
+    };
 
-    function activate() {
+    var addMarker = function(latlng) {
+      $scope.markers.push({
+        lat: latlng.lat,
+        lng: latlng.lng
+      });
+    };
 
-    }
+    var setView = function(latlng) {
+      leafletData.getMap('map').then(
+        function(map) {
+          map.setView(latlng);
+        }
+      );
+    };
+
+    var drawRoutes = function(routes) {
+      clearRoutes();
+
+      routes.type = 'multiPolyline';
+      routes.weight = 4;
+      routes.color = 'red';
+      routes.opacity = 0.5;
+      $scope.paths.multiPolyline = routes;
+
+      var lastPoint = routes.latlngs[0][routes.latlngs[0].length - 1];
+      addMarker(lastPoint);
+      setView(lastPoint);
+    };
+
+    drawRoutes(routes);
   }
 })();
