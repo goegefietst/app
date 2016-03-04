63120a1a978e05a8eb709a7cb7f9d1760ee6245b
Wiebe Marichael <wiebe318@gmail.com>
2016-03-04 14:35:42 +0100

    #5 Added controller + functions navigation timespan

--- www/app/performance/performance-personal/personal-performance.controller.js
+++ www/app/performance/performance-personal/personal-performance.controller.js
@@ -16,5 +16,30 @@
     function activate() {
 
     }
+
+    vm.day = function day() {
+      //change timespan to day
+      console.log('test timespan changed to day');
+    }
+
+    vm.week = function week() {
+      //change timespan to week
+      console.log('test timespan changed to week');
+    }
+
+    vm.month = function month() {
+      //change timespan to month
+      console.log('test timespan changed to month');
+    }
+
+    vm.previous = function previous() {
+      //select the previous day/week/year
+      console.log('test previous was clicked');
+    }
+
+    vm.next = function next() {
+      //select the next day/week/year
+      console.log('test next was clicked')
+    }
   }
 })();
