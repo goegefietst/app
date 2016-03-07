2b4b8a35afc747b029470b15c40f69b76b040e79
Wiebe Marichael <wiebe318@gmail.com>
2016-03-07 14:12:57 +0100

    #10 add fuctions to delete/edit reminders

--- www/app/settings/settings.controller.js
+++ www/app/settings/settings.controller.js
@@ -22,19 +22,44 @@
     vm.masterCheck = true;
     vm.testReminders = [
       {
+        id: 0,
         check: true,
         days: 'iedere dag',
         time: '7:20'
       },{
+        id: 1,
         check: false,
         days: 'zaterdag',
         time: '12:00'
       }
+      ,{
+        id: 2,
+        check: true,
+        days: 'iedere dag',
+        time: '12:30'
+      }
     ];
-    vm.test = function test() {
+
+    vm.add = function add() {
       console.log('test add');
     }
 
+    vm.onItemDelete = function onItemDelete(reminder) {
+      console.log('test delete');
+      console.log(reminder.id);
+      
+    }
+
+    vm.onItemEdit = function onItemEdit(reminder) {
+      console.log('test edit');
+      console.log(reminder);
+    }
+
+    vm.printReminders = function printReminders(){
+      for(var i = testReminders.length - 1; i >= 0; i--) {
+        console.log(testReminders[i].id);
+      }
+    }
 
   }
 })();
