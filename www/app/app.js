2e58f5ab894b91e50aa66071e871df7b64d3f28d
Pieter-Jan Vandenberghe <pieterjan.vandenberghe.t6365@student.hogent.be>
2016-04-11 10:23:39 +0200

    #2

--- www/app/app.js
+++ www/app/app.js
@@ -7,10 +7,12 @@
     'app.performance',
     'app.settings',
     'app.layout',
-    'ionic-timepicker'
+    'ionic-timepicker',
+    'database',
+    'connection'
   ])
 
-  .run(function($ionicPlatform) {
+  .run(function($ionicPlatform, $cordovaNetwork, $window, $q, Database, Connection) {
     $ionicPlatform.ready(function() {
       // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
       // for form inputs)
@@ -23,6 +25,58 @@
         // org.apache.cordova.statusbar required
         StatusBar.backgroundColorByHexString('#f07b47');
       }
+    });
+    $ionicPlatform.on('pause', function() {
+      if (
+        $cordovaNetwork.getNetwork() !== 'wifi' ||
+        $cordovaNetwork.isOnline() === false
+      ) {
+        return;
+      }
+      var uuid = $window.localStorage.getItem('uuid');
+      var secret = $window.localStorage.getItem('secret');
+      if (uuid === null || secret === null) {
+        Connection.makeAccount().then(function(data) {
+          uuid = data.uuid;
+          secret = data.secret;
+          $window.localStorage.setItem('uuid', data.uuid);
+          $window.localStorage.setItem('secret', data.secret);
+        });
+      }
+
+      var callback = function(routes) {
+        if (Object.prototype.toString.call(routes) !== '[object Array]' ||
+          routes.length < 1) {
+          console.log('ERROR: ROUTES IS NOT AN ARRAY OR AN EMPTY ARRAY');
+          console.log(routes);
+          return;
+        }
+        for (var i = 0; i < routes.length; i++) {
+          var callback = function(points) {
+            var route = routes[i];
+            route.points = points;
+            Connection.postRoute(uuid, secret, route).then(function() {
+              console.log('Sent a route to the server');
+              console.log(route);
+              Database.sentRoute(route);
+            });
+          };
+          Database.selectPoints(callback, [routes[i].id]);
+
+        }
+      };
+      var options = {
+        sent: true
+      };
+      Database.selectRoutes(callback, options);
+
+      /*
+      FOR TESTING PURPOSES
+      Connection.getRoutes().then(function(d) {
+        console.log('DATA');
+        console.log(d);
+      });
+      */
     });
   });
 })();
