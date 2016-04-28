b06a51387343c3a37f75a3c82ef280bf7ee2dd12
Wiebe Marichael <wiebe318@gmail.com>
2016-04-28 16:46:59 +0200

    #27 setup list top 5

--- www/app/group/group.controller.js
+++ www/app/group/group.controller.js
@@ -10,5 +10,32 @@
   /* @ngInject */
   function Controller() {
     var vm = this;
+    vm.schools = [
+      {
+        nr: 1,
+        school: 'HoGent',
+        km: '324,5'
+      },
+      {
+        nr: 2,
+        school: 'uGent',
+        km: '302,8'
+      },
+      {
+        nr: 3,
+        school: 'Artevelde',
+        km: '267,1'
+      },
+      {
+        nr: 4,
+        school: 'Odisee',
+        km: '120,4'
+      },
+      {
+        nr: 5,
+        school: 'Luca',
+        km: '103,9'
+      }
+    ];
   }
 })();
