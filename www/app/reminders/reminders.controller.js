0e08de7a4f889018f7854581de72575231518567
Pieter-Jan Vandenberghe <pieterjan.vandenberghe.t6365@student.hogent.be>
2016-08-12 16:13:53 +0200

    #41 reminders controller move documentation to each function

--- www/app/reminders/reminders.controller.js
+++ www/app/reminders/reminders.controller.js
@@ -23,66 +23,11 @@
     vm.showEdit = false;
     vm.enabled = Reminders.isEnabled();
 
-    /**
-     * @ngdoc method
-     * @name addReminder
-     * @methodOf app.reminders.controller:RemindersController
-     * @description
-     * Opens popup to add a reminder
-     * See {@link app.reminders.service:RemindersService ReminderService#addReminder()}
-     */
     vm.addReminder = addReminder;
-
-    /**
-     * @ngdoc method
-     * @name editReminder
-     * @methodOf app.reminders.controller:RemindersController
-     * @description
-     * Opens popup to edit a reminder
-     * See {@link app.reminders.service:RemindersService ReminderService#editReminder()}
-     * @param {Object} reminder reminder that is to be edited
-     */
     vm.editReminder = editReminder;
-
-    /**
-     * @ngdoc method
-     * @name toggleDelete
-     * @methodOf app.reminders.controller:RemindersController
-     * @description
-     * Toggles display of delete button next to every reminder
-     */
     vm.toggleDelete = toggleDelete;
-
-    /**
-     * @ngdoc method
-     * @name deleteReminder
-     * @methodOf app.reminders.controller:RemindersController
-     * @description
-     * Delete a reminder
-     * See {@link app.reminders.service:RemindersService ReminderService#deleteReminder()}
-     * @param {Object} reminder reminder that is to be deleted
-     */
     vm.deleteReminder = deleteReminder;
-
-    /**
-     * @ngdoc method
-     * @name toggleReminder
-     * @methodOf app.reminders.controller:RemindersController
-     * @description
-     * Toggle whether a reminder is active or not
-     * See {@link app.reminders.service:RemindersService ReminderService#toggleReminder()}
-     * @param {Object} reminder reminder that is to enabled/disabled
-     */
     vm.toggleReminder = toggleReminder;
-
-    /**
-     * @ngdoc method
-     * @name toggleEnabled
-     * @methodOf app.reminders.controller:RemindersController
-     * @description
-     * Toggle whether all reminders are enabled
-     * See {@link app.reminders.service:RemindersService ReminderService#setEnabled()}
-     */
     vm.toggleEnabled = toggleEnabled;
 
     loadReminders();
@@ -93,26 +38,76 @@
       });
     }
 
+    /**
+     * @ngdoc method
+     * @name addReminder
+     * @methodOf app.reminders.controller:RemindersController
+     * @description
+     * Opens popup to add a reminder
+     * See {@link app.reminders.service:RemindersService ReminderService#addReminder()}
+     */
     function addReminder() {
       Reminders.addReminder($scope, vm.reminders);
     }
 
+    /**
+     * @ngdoc method
+     * @name editReminder
+     * @methodOf app.reminders.controller:RemindersController
+     * @description
+     * Opens popup to edit a reminder
+     * See {@link app.reminders.service:RemindersService ReminderService#editReminder()}
+     * @param {Object} reminder reminder that is to be edited
+     */
     function editReminder(reminder) {
       Reminders.editReminder($scope, vm.reminders, reminder);
     }
 
+    /**
+     * @ngdoc method
+     * @name toggleDelete
+     * @methodOf app.reminders.controller:RemindersController
+     * @description
+     * Toggles display of delete button next to every reminder
+     */
     function toggleDelete() {
       vm.showDelete = !vm.showDelete;
     }
 
+    /**
+     * @ngdoc method
+     * @name deleteReminder
+     * @methodOf app.reminders.controller:RemindersController
+     * @description
+     * Delete a reminder
+     * See {@link app.reminders.service:RemindersService ReminderService#deleteReminder()}
+     * @param {Object} reminder reminder that is to be deleted
+     */
     function deleteReminder(reminder) {
       vm.showDelete = Reminders.deleteReminder(reminder, vm.reminders);
     }
 
+    /**
+     * @ngdoc method
+     * @name toggleReminder
+     * @methodOf app.reminders.controller:RemindersController
+     * @description
+     * Toggle whether a reminder is active or not
+     * See {@link app.reminders.service:RemindersService ReminderService#toggleReminder()}
+     * @param {Object} reminder reminder that is to enabled/disabled
+     */
     function toggleReminder(reminder) {
       Reminders.toggleReminder(reminder, vm.reminders);
     }
 
+    /**
+     * @ngdoc method
+     * @name toggleEnabled
+     * @methodOf app.reminders.controller:RemindersController
+     * @description
+     * Toggle whether all reminders are enabled
+     * See {@link app.reminders.service:RemindersService ReminderService#setEnabled()}
+     */
     function toggleEnabled() {
       vm.enabled = !vm.enabled;
       Reminders.setEnabled(vm.enabled, vm.reminders);
